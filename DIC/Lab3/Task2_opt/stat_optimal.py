a881b2044c72b47c378e8a684e143ada9ea1563a
SmlCoke <j.feng.st05@gmail.com>
2025-11-28 16:36:21 +0800

    DIC Lab3 remove trash files(Task2)

--- DIC/Lab3/Task2_opt/stat_optimal.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-if __name__ == "__main__":
-    # 读取csv文件，找到tp列最小的那一行对应的所有值
-    df = pd.read_csv("task2_opt.csv")
-    min_tp_row = df.loc[df['tp'].idxmin()]
-    print("Optimal parameters:")
-    print(min_tp_row)
